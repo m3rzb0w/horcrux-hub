dc69e5135eeb80155acd49bb97964b69dd4fb331
m3rzb0w <80rN70c0D3@protonmail.com>
2023-12-20 08:01:36 +0000

    up chat

--- front/src/components/Chat.vue
+++ front/src/components/Chat.vue
@@ -1,4 +1,41 @@
 <script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+interface Message {
+    username: string,
+    message: string
+}
+
+const userName = ref<string>("")
+const userMessage = ref<string>("")
+const allMessages = reactive<Array<Message>>([]);
+const socket = new WebSocket(import.meta.env.VITE_WS_CHAT)
+
+socket.onmessage = (event) => {
+    allMessages.push(JSON.parse(event.data))
+}
+
+const userNameCheck = () => {
+    if (userName.value.trim() == "") {
+        const num = Math.floor(Math.random() * 10000)
+        userName.value = "anon" + num
+    }
+}
+
+const sendMessage = () => {
+    if (userMessage.value.trim().length !== 0) {
+        userNameCheck()
+        const newMessage: Message = {
+            username: userName.value,
+            message: userMessage.value,
+        }
+        socket.send(JSON.stringify(newMessage))
+
+        userMessage.value = ""
+    }
+}
+
+
 </script>
 
 <template>
@@ -7,6 +44,24 @@
             <h1 class="wip">
                 WIP
             </h1>
+            <div>
+
+                <h2>All Messages</h2>
+                <ul>
+                    <li v-for="(message, index) in allMessages" :key="index">
+                        <strong>{{ message.username }}</strong>: {{ message.message }}
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <label for="username">Username:</label>
+                <input type="text" id="username" v-model="userName" />
+            </div>
+            <div>
+                <label for="message">Message:</label>
+                <input type="text" id="message" v-model="userMessage" />
+            </div>
+            <button @click="sendMessage">Add Message</button>
         </div>
     </div>
 </template>
